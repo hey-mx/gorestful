672828dd3b6b12c4e8fcf3f60eabbff14cbf2c8e
Juan Carlos Juárez <isc.jcjl@me.com>
2016-08-25 23:03:05 -0500

    Implementing logic in order to save the user information to disk

--- User.go
+++ User.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/base64"
+	"encoding/json"
+	"os"
+)
+
 //User struct mode
 type User struct {
 	Name string `json:"name"`
@@ -8,3 +14,21 @@ type User struct {
 
 //Users group of users
 type Users []User
+
+//SaveUser method for write user information to disk
+func SaveUser(userInfo *User) string {
+	userid := base64.StdEncoding.EncodeToString([]byte(userInfo.Name))
+	fi, err := os.Create("/tmp/" + userid)
+	defer fi.Close()
+	if err != nil {
+		println(err)
+	} else {
+		if jsonStr, err := json.Marshal(userInfo); err == nil {
+			_, err := fi.Write([]byte(jsonStr))
+			if err != nil {
+				userid = ""
+			}
+		}
+	}
+	return userid
+}
